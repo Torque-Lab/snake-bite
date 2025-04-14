6732a5abeb786240071b14e57b7d79dbe568fca1
Mathura <Torque-Lab@users.noreply.github.com>
2025-04-14 18:09:00 +0530

    initalize asset

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let direction = { x: 0, y: 0 };
+
+let foodSound = new Audio("food.mp3");
+
+let gameOverSound = new Audio("game-over.mp3");
+let moveSound = new Audio("");
+let musicSound = new Audio("bg-music.mp3");
+
+
+//Game function
+
+
+
+
+
+
+
+
+window.requestAnimationFrame(main);
